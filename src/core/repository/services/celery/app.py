f6ddda109c348f422fe964b2f51534ae4ddece49
RuslanChukavin <r66cha@gmail.com>
2025-09-28 16:57:59 +0300

    add comment celery_app

--- src/core/repository/services/celery/app.py
+++ src/core/repository/services/celery/app.py
@@ -13,6 +13,7 @@ __all__ = ["celery_app"]
 
 TASKS_DIR = "src.core.repository.tasks"
 
+# Можно также использовать Redis в качестве брокера
 celery_app = Celery(
     "my_app",
     broker="amqp://guest:guest@rabbitmq:5672//",
